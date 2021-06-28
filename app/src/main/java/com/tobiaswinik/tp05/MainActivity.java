7150a8f04660dd09af61aaea650bbe809dabdeac
Tobias <tobiwinik@gmail.com>
2021-06-28 19:45:32 -0300

    commit de prueba #1

--- app/src/main/java/com/tobiaswinik/tp05/MainActivity.java
+++ app/src/main/java/com/tobiaswinik/tp05/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     NameFragment fragmentName;
     GameFragment fragmentGame;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
